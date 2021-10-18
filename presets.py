0f9ee7950fda7ea66c88060a998cc8aaee7660ad
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-10-18 21:27:29 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -3,18 +3,18 @@
 class Presets(object):
     CAPTION_TEXT_DOC = "\n\n<b>File Name:</b> {}\n\n<b>Format:</b> {}\n<b>Size:</b> {}"
     CAPTION_TEXT_VID = "\n\n<b>File Name:</b> {}\n\n<b>Size:</b> {}"
-    ASK_PM_TEXT = "<b>Click the below button</b>"
-    WELCOME_TEXT = "Hello.. <b>{}</b>\n<code>I can help you getting movies from</code> @MovieKeralam. " \
-                   "<code>Just Keep this message live Here</code>😉\n\n" \
+    ASK_PM_TEXT = "<b>Hey <b>{}<\b>👇നിങ്ങൾ ചോദിച്ച മൂവി ലഭിക്കാൻ താഴെ കാണുന്ന ബട്ടണിൽ ക്ലിക്ക് ചെയ്യുക😇</b>"
+    WELCOME_TEXT = "Hello.. <b>{}</b>\n<code> I'm a single auto filter botAdd me to your group and make me an admin and join </code> @tg_bots_disccurssions. " \
+                   "<code>Then see my pevers✨</code>😉\n\n" \
                    "<b>My code can be seen: </b><a href='https://github.com/m4mallu/gofilesbot'> HERE</a>"
     CLEAN_CHAT_MSG = "⚠️ <b>Deleting all messages..</b>"
     MSG_FOR_PIN = "<b>For getting medias from here..</b>\n\n🔛 <code>Please start</code> @{} <code>in PM\n\n" \
                   "Send the exact Movie name.\n\n🔊 I'll reply the file in PM if available in our channel !</code>"
 
-    BOT_PM_TEXT = "<b>Sorry.. 😢</b>\n\n<code>Bot won't work in PM, Ask in ma Group. I'll reply the file in PM if " \
+    BOT_PM_TEXT = "<b>Sorry.. 😢</b>\n\n<code>Bot pm il work avulla movie venamennundenkil groupil poi chodikku 😜" \
                   "available in our DB !</code>"
-    PM_ERROR = "<b>Unable to send medias</b> ⛔️\n<code>Click the below button\nAsk here for movies later!</code>"
-    MEDIA_SEND_TEXT = "<code>Media dispatched as PM 🥳</code>"
+    PM_ERROR = "<b>Thaankal choicha file enikku pm send cheyyan kazhinjilla</b> ⛔️\n<code>Thazhe kaanunna button click cheythu bot start cheyu\nAsk here for movies later!</code>"
+    MEDIA_SEND_TEXT = "<code>Hey <b>{}<\b> THAANKAL CHODICHA FILE PM SEND CHEYTHITTUNDE THAZHE KAANJNNA BITTON CLICK CHYTHU DOWNLOAD CHEYYU 🥳</code>"
     NO_MEDIA = "Requested movie: <b>{}</b>\n\n<b>Not available " \
                "Right Now</b>\n<code>Possible Causes : 🤔\n\n⭕️ Not " \
                "released yet</code>\n⭕️ <a href='https://www.google.com/search?q={}'> Spelled incorrectly</a>\n" \
